6ae060503bd37e3105e5c3800a01cab61c543a3e
melissa billard <billardmelissa1@gmail.com>
2024-10-16 10:01:49 +0200

    add page dash route

---
+++ src/Controller/DashHomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DashHomeController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashhome')]
+    public function index(): Response
+    {
+        return $this->render('dashboard/dashboard.html.twig', [
+            'controller_name' => 'DashHomeController',
+        ]);
+    }
+}
