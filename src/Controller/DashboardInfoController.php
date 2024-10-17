47e850db22d02c42c319ae07574bd2a15c7ec751
ritacarrilho <ritaclameira@gmail.com>
2024-10-17 09:44:00 +0200

    added image blur and fix login

--- src/Controller/DashboardInfoController.php
+++ src/Controller/DashboardInfoController.php
@@ -11,9 +11,6 @@ class DashboardInfoController extends AbstractController
     #[Route('/dashboard/enfants', name: 'app_dashboard_info')]
     public function index(): Response
     {
-        // get parent info : firstname, lastaname, email, phone, birthday, address
-        // get child
-
         return $this->render('dashboard_info/dashboardinfo.html.twig', [
             'controller_name' => 'DashboardInfoController',
         ]);
